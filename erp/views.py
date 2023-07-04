43ff7cef7373427b123f3b4d9d9691df91f34ff1
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-07-04 13:06:25 -0300

    init

--- erp/views.py
+++ erp/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+#  Camada view:
+#  Prover o roteamento de rotas,
+#  Processar Requisições HTTP,
+#  Aplicar as regras de negócio definidas para cada view
+#  Formular Respostas HTTP, Recepcionar requisições http, Processa - lás e responde -  las
+
+
+# Objeto HttpRequest
+
+# Atributos:
+
+# scheme  ->  Ex: http/https
+# path    ->  Ex: "/funcionarios/cadastrar"
+# method  ->  Ex: GET/POST
+# headers ->  Contém o cabeçalho HTTP
+# FILES  ->   Contém arquivos, caso algum tenha sido enviado.
+
+
+# Objeto HttpResponse
+
+# Atributos:
+
+# Content -> Payload da Resposta
+# headers: Cabeçalhos HTTP
+# status_code: Código de status HTT
+
+
+
+
+
+
+
+
+
+
+
+
