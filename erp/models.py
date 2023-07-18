490f21204b5aa8a4cad1e713e961855fc9ccaba6
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-07-18 10:32:58 -0300

    View Atualização de venda

--- erp/models.py
+++ erp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-#                                           null que ele deve ser preenchido, não pode ter um valor nulo no DB
-#                                           blank não pode ser deixado em branco ao ser preenchido em um formulário
+# null que ele deve ser preenchido, não pode ter um valor nulo no DB
+# blank não pode ser deixado em branco ao ser preenchido em um formulário
 
 
 class Funcionario(models.Model):
@@ -12,12 +12,17 @@ class Funcionario(models.Model):
     email_funcional = models.EmailField(max_length=60, null=False, blank=False)
     remuneracao = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False)
 
+    def __str__(self):
+        return f'ID {self.id} - {self.nome} {self.sobrenome}'
+
 
 class Produto(models.Model):
     nome = models.CharField(max_length=50, null=False, blank=False)
     descricao = models.CharField(max_length=255, null=False, blank=False)
     preco = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
 
+    def __str__(self):
+        return f'ID {self.id} - {self.nome} (Preço: R$ {self.preco})'
 
 class Venda(models.Model):
     funcionario = models.ForeignKey('Funcionario', on_delete=models.CASCADE)
--- erp/views.py
+++ erp/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect, Http404
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, UpdateView
-from erp.forms import FuncionarioForm, ProdutoForm
-from .models import Funcionario, Produto
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DetailView, DeleteView
 
+from erp.forms import FuncionarioForm, ProdutoForm
+from .models import Funcionario, Produto, Venda
 
 class HomeView(TemplateView):
     template_name = 'erp/index.html'
 
 
-
 def cria_funcionario(requisicao: HttpRequest):
-    if requisicao.method == 'GET':                 # Se variável.método GET
-        form = FuncionarioForm()                   # var form = classe FuncionarioForm e seus campos de formulário forms.py
+    if requisicao.method == 'GET':  # Se variável.método GET
+        form = FuncionarioForm()  # var form = classe FuncionarioForm e seus campos de formulário forms.py
         return render(requisicao, template_name='erp/funcionarios/novo.html', context={"form": form})
 
 
-    elif requisicao.method == 'POST':               # Envia dados
-        form = FuncionarioForm(requisicao.POST)     # envia todos os dados do formulário preenchido
+    elif requisicao.method == 'POST':  # Envia dados
+        form = FuncionarioForm(requisicao.POST)  # envia todos os dados do formulário preenchido
 
-        if form.is_valid():                         # se meu formulário e seus campos foram preenchido corretamente.
-                                                    # var funcionario = classe Funcionario de models.py
+        if form.is_valid():  # se meu formulário e seus campos foram preenchido corretamente.
+            # var funcionario = classe Funcionario de models.py
 
             funcionario = Funcionario(
 
@@ -31,22 +30,21 @@ def cria_funcionario(requisicao: HttpRequest):
                 email_funcional=form.cleaned_data.get('email_funcional'),
                 remuneracao=form.cleaned_data.get('remuneracao'),
             )
-             # o Django armazena os dados válidos no dicionário cleaned_data
+            # o Django armazena os dados válidos no dicionário cleaned_data
 
             funcionario.save()
 
-            return HttpResponseRedirect(redirect_to='/') # barra quer dizer que voltamos para raiz da app
-
-
+            return HttpResponseRedirect(redirect_to='/')  # barra quer dizer que voltamos para raiz da app
 
 
 def lista_funcionarios(requisicao: HttpRequest):
     if requisicao.method == 'GET':
         funcionarios = Funcionario.objects.all()
 
-        return render(requisicao, template_name='erp/funcionarios/list.html',  context={"funcionarios": funcionarios})
+        return render(requisicao, template_name='erp/funcionarios/lista.html', context={"funcionarios": funcionarios})
 
-def busca_funcionario_por_id(requisicao: HttpRequest, pk: int):    # 1
+
+def busca_funcionario_por_id(requisicao: HttpRequest, pk: int):  # 1
     if requisicao.method == 'GET':
         try:
             funcionario = Funcionario.objects.get(pk=pk)
@@ -56,7 +54,7 @@ def busca_funcionario_por_id(requisicao: HttpRequest, pk: int):    # 1
 
         return render(requisicao, template_name='erp/funcionarios/detalhe.html', context={"funcionario": funcionario})
 
-    
+
 def atualiza_funcionario(requisicao: HttpRequest, pk: int):
     if requisicao.method == 'GET':
         funcionario = Funcionario.objects.get(pk=pk)
@@ -67,26 +65,26 @@ def atualiza_funcionario(requisicao: HttpRequest, pk: int):
     elif requisicao.method == 'POST':
         funcionario = Funcionario.objects.get(pk=pk)
         form = FuncionarioForm(requisicao.POST, instance=funcionario)
-         # fomulario atualizado com a chave cadastrada anteriormente
+        # fomulario atualizado com a chave cadastrada anteriormente
 
         if form.is_valid():
             form.save()
 
-            return HttpResponseRedirect(redirect_to=f'/funcionarios/detalhe/{pk}') # retornou para url detalhe
+            return HttpResponseRedirect(redirect_to=f'/funcionarios/detalhe/{pk}')  # retornou para url detalhe
 
 
 class ProdutoCreateView(CreateView):
-    template_name = 'erp/produtos/novo.html'
     model = Produto
+    template_name = 'erp/produtos/novo.html'
     form_class = ProdutoForm
     success_url = reverse_lazy('erp:home')
 #            nome do app em urls.py:nome da view
 
 
 class ProdutoListView(ListView):
-    template_name = 'erp/produtos/list.html'
+    template_name = 'erp/produtos/lista.html'
     model = Produto
-    context_object_name = 'produtos'
+    context_object_name = 'produtos' # variável de contexto que podemos manipular no template
 
 
 class ProdutoUpdateView(UpdateView):
@@ -94,3 +92,74 @@ class ProdutoUpdateView(UpdateView):
     template_name = 'erp/produtos/atualiza.html'
     form_class = ProdutoForm
     success_url = reverse_lazy('erp:lista_produtos')
+
+
+class ProdutoDetailView(DetailView):
+    model = Produto
+    template_name = 'erp/produtos/detalhe.html'
+    context_object_name = 'produto'        # variável de contexto que podemos manipular no template
+
+
+    def get_object(self, queryset=None):
+        """ Método responsável por obter um objeto específico
+            do modelo Produto a partir de um conjunto de dados. """
+
+        # Tenta executar o seguinte código:
+        try:
+            # Chama o método get_object() da superclasse (DetailView) usando a função super().
+            return super().get_object(queryset)
+            # Isso executa a lógica padrão de obtenção do objeto e retorna o objeto encontrado.
+
+        # Se ocorrer uma exceção Http404 (página não encontrada), entra nesse bloco:
+        except Http404:
+            return None
+            # Retorna None, indicando que nenhum objeto foi encontrado.
+
+
+
+class ProdutoDeleteView(DeleteView):
+    model = Produto
+    template_name = 'erp/produtos/deleta.html'
+    context_object_name = 'produto'
+    success_url = reverse_lazy('erp:lista_produtos')
+
+    def get_object(self, queryset=None):
+        try:
+            return super().get_object(queryset)
+
+        except Http404:
+            return None
+
+
+class VendaCreateView(CreateView):
+    model = Venda
+    template_name = 'erp/vendas/novo.html'
+    success_url = reverse_lazy('erp:home')
+    fields = ['funcionario', 'produto']
+
+
+class VendaListView(ListView):
+    model = Venda
+    template_name = 'erp/vendas/lista.html'
+    context_object_name = 'vendas'
+
+
+
+class VendaDetailView(DetailView):
+    model = Venda
+    template_name = 'erp/vendas/detalhe.html'
+    context_object_name = 'venda'
+
+    def get_object(self, queryset=None):
+        try:
+            return super().get_object(queryset)
+
+        except Http404:
+            return None
+
+
+class VendaUpdateView(UpdateView):
+    model = Venda
+    template_name = 'erp/vendas/atualiza.html'
+    fields = '__all__'
+    success_url = reverse_lazy('erp:lista_vendas')
